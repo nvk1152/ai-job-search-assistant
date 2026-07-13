19724db653e7c94025bb9b364c6e1b825246c85e
nvk1152 <nvk1152@gmail.com>
2026-07-13 11:41:08 +0530

    Initial Commit

---
+++ src/main/java/com/nvk/jsa/AiJobSearchAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.nvk.jsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiJobSearchAssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiJobSearchAssistantApplication.class, args);
+    }
+
+}
